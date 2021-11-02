5fe8b00ffe5bd39220e263e0d5d1721b8c290f28
Anthony J Mirabella <a9@aneurysm9.com>
2021-11-02 19:39:21 -0400

    go: make lambda_handler() a closure to avoid constantly creating s3/http clients
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>

--- go/sample-apps/function/function.go
+++ go/sample-apps/function/function.go
@@ -24,7 +24,7 @@ import (
 	"go.opentelemetry.io/otel"
 )
 
-func lambda_handler(ctx context.Context) (interface{}, error) {
+func lambda_handler(ctx context.Context) func(ctx context.Context) (interface{}, error) {
 	// init aws config
 	cfg, err := awsConfig.LoadDefaultConfig(ctx)
 	if err != nil {
@@ -36,50 +36,53 @@ func lambda_handler(ctx context.Context) (interface{}, error) {
 
 	// S3
 	s3Client := s3.NewFromConfig(cfg)
-	input := &s3.ListBucketsInput{}
-	result, err := s3Client.ListBuckets(ctx, input)
-	if err != nil {
-		fmt.Printf("Got an error retrieving buckets, %v", err)
-	}
-
-	fmt.Println("Buckets:")
-	for _, bucket := range result.Buckets {
-		fmt.Println(*bucket.Name + ": " + bucket.CreationDate.Format("2006-01-02 15:04:05 Monday"))
-	}
-	fmt.Println("End Buckets.")
-
 	// HTTP
 	httpClient := &http.Client{
 		Transport: otelhttp.NewTransport(
 			http.DefaultTransport,
 		),
 	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.github.com/repos/open-telemetry/opentelemetry-go/releases/latest", nil)
-	if err != nil {
-		fmt.Printf("failed to create http request, %v\n", err)
-	}
-	res, err := httpClient.Do(req)
-	if err != nil {
-		fmt.Printf("failed to make http request, %v\n", err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
+
+	return func(ctx context.Context) (interface{}, error) {
+		input := &s3.ListBucketsInput{}
+		result, err := s3Client.ListBuckets(ctx, input)
 		if err != nil {
-			fmt.Printf("failed to close http response body, %v\n", err)
+			fmt.Printf("Got an error retrieving buckets, %v", err)
 		}
-	}(res.Body)
 
-	var data map[string]interface{}
-	err = json.NewDecoder(res.Body).Decode(&data)
-	if err != nil {
-		fmt.Printf("failed to read http response body, %v\n", err)
+		fmt.Println("Buckets:")
+		for _, bucket := range result.Buckets {
+			fmt.Println(*bucket.Name + ": " + bucket.CreationDate.Format("2006-01-02 15:04:05 Monday"))
+		}
+		fmt.Println("End Buckets.")
+
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.github.com/repos/open-telemetry/opentelemetry-go/releases/latest", nil)
+		if err != nil {
+			fmt.Printf("failed to create http request, %v\n", err)
+		}
+		res, err := httpClient.Do(req)
+		if err != nil {
+			fmt.Printf("failed to make http request, %v\n", err)
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				fmt.Printf("failed to close http response body, %v\n", err)
+			}
+		}(res.Body)
+
+		var data map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&data)
+		if err != nil {
+			fmt.Printf("failed to read http response body, %v\n", err)
+		}
+		fmt.Printf("Latest OTel Go Release is '%s'\n", data["name"])
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       os.Getenv("_X_AMZN_TRACE_ID"),
+		}, nil
 	}
-	fmt.Printf("Latest OTel Go Release is '%s'\n", data["name"])
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Body:       os.Getenv("_X_AMZN_TRACE_ID"),
-	}, nil
 }
 
 func main() {
@@ -99,7 +102,7 @@ func main() {
 	otel.SetTracerProvider(tp)
 	otel.SetTextMapPropagator(xray.Propagator{})
 
-	lambda.Start(otellambda.InstrumentHandler(lambda_handler, xrayconfig.EventToCarrier(), otellambda.WithFlusher(tp)))
+	lambda.Start(otellambda.InstrumentHandler(lambda_handler(ctx), xrayconfig.EventToCarrier(), otellambda.WithFlusher(tp)))
 }
 
 func initTracerProvider(ctx context.Context) (*sdktrace.TracerProvider, error) {
